05abe69ef1245df1b809139a4e2ba87c53f132ed
diablo <progzone122@gmail.com>
2024-10-09 15:53:18 +0300

    Update built files

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 			fallback: 'index.html'
 		}),
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/fkrkm-journal.github.io' : '',
+            base: process.env.NODE_ENV === 'production' ? '/fkrkm-journal' : '',
         }
 	},
 };
